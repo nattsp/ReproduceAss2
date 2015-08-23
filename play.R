0de784174511609403bc91ac9c8dfd0488c2a686
nattsp <drnphillips@gmail.com>
2015-08-23 21:02:01 +1000

    Ready to write up

--- play.R
+++ play.R
@@ -1,6 +1,9 @@
 #example assignment
 #http://www.rpubs.com/rdpeng/13396
 library(dplyr)
+library(ggplot2)
+library(tidyr)
+options(scipen=9)
 #Download data
 if (!file.exists("data")){
     dir.create("data")
@@ -35,6 +38,25 @@ harmful <- select(storms,
     arrange(desc(Fatalities), desc(Injuries))
 head(harmful)
 
+harmful10 <- harmful[1:10, ]
+
+harmfultall <- gather(harmful10, TypeOfHarm, Numbers, Fatalities:Injuries)
+harmfultall$EVTYPE <- factor(harmfultall$EVTYPE,
+                           levels = harmful10$EVTYPE, ordered = TRUE)
+
+
+g <- ggplot(data = harmfultall, aes(x = EVTYPE, y = Numbers, fill = TypeOfHarm))
+g <- g + geom_bar(stat="identity", position="dodge")
+g <- g + coord_cartesian(ylim=c(0,20000))
+g <- g + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+g <- g + labs(title = "Bar chart of deaths and injuries by event type")
+g <- g + labs(x = "Event Type", y = "Number of injuries over 20,000 not shown")
+
+g
+
+
+
+
 harmful2 <- group_by(harmful, EVTYPE,) %>%
     summarise(Fatalies = sum(FATALITIES))
 
@@ -45,10 +67,9 @@ table(storms$PROPDMGEXP)
 table(storms$CROPDMGEXP)
 
 convertPROPDMGEXP <- data.frame(PROPDMGEXP = c("H", "K", "M", "B", ""), 
-                           PROPDMGfactor = c(100, 1000, 1000000, 1000000000, ""))
+                           PROPDMGfactor = c(100, 1000, 1000000, 1000000000, 0))
 convertCROPDMGEXP <- data.frame(CROPDMGEXP = c("H", "K", "M", "B", ""), 
-                                PROPDMGfactor = c(100, 1000, 1000000, 1000000000, ""))
-
+                                CROPDMGfactor = c(100, 1000, 1000000, 1000000000, 0))
 
 damage <- select(storms,
                 EVTYPE,
@@ -62,16 +83,66 @@ damage <- select(storms,
     filter(PROPDMGEXP %in% c("H", "K", "M", "B", "") &
                      CROPDMGEXP %in% c("H", "K", "M", "B", "")) %>%
     left_join(convertPROPDMGEXP, c("PROPDMGEXP")) %>%
-    left_join(convertCROPDMGEXP, c("CROPDMGEXP"))
+    left_join(convertCROPDMGEXP, c("CROPDMGEXP")) %>%
+    mutate(PropertyCosts = PROPDMG * PROPDMGfactor,
+           CropCosts = CROPDMG * CROPDMGfactor,
+           TotalDamage = PropertyCosts + CropCosts) %>%
+    arrange(desc(TotalDamage), desc(PropertyCosts), desc(CropCosts))
 
 
 head(damage)
-damage <- left_join(damage, convertCROPDMGEXP, c("CROPDMGEXP"))
 
-damage <- filter(damage, PROPDMGEXP %in% c("H", "K", "M", "B") |
-                     CROPDMGEXP %in% c("H", "K", "M", "B"))
-damage <- filter(damage, PROPDMGEXP %in% c("H", "K", "M", "B", "") &
-                     CROPDMGEXP %in% c("H", "K", "M", "B", ""))
+damageEVTYPE <- damage %>%
+    select(EVTYPE, TotalDamage) %>%
+    group_by(EVTYPE) %>%
+    summarize(TotalDamage = sum(TotalDamage)) %>%
+    arrange(desc(TotalDamage))
+
+
+
+EVTYPETop10 <- damageEVTYPE[1:10, ]$EVTYPE
+
+damageTotalTop10 <- damageEVTYPE[1:10, ]
+
+g <- ggplot(data = damageTotalTop20, aes(x = EVTYPE, y = TotalDamage))
+g <- g + geom_bar(stat="identity")
+g <- g + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+g <- g + labs(title = "Bar chart showing  costs for 10 events with greatest economic consequency")
+g <- g + labs(x = "Event Type", y = "Log of costs")
+
+g
+
+##Look at the range of costs for the largest damages
+damageTop10 <- filter(damage, EVTYPE %in% EVTYPETop10) %>%
+    select(EVTYPE, PropertyCosts, CropCosts)
+
+damageTop10$EVTYPE <- factor(damageTop10$EVTYPE,
+                             levels = EVTYPETop10, ordered = TRUE)
+
+damageTop10 <- gather(damageTop10, DamageType, Dollars, PropertyCosts:CropCosts)
+
+
+#distinct(select(damage, EVTYPE))
+
+
+
+g <- ggplot(data = damageTop10, aes(x = EVTYPE, y = log10(Dollars + 1), fill = DamageType))
+g <- g + geom_boxplot()
+#g <- g + coord_flip()
+g <- g + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+g <- g + labs(title = "Box plot showing distribution of Log of damage costs for 10 events with greatest economic consequency")
+g <- g + labs(x = "Event Type", y = "Log of costs")
+
+g
+
+
+
+g <- ggplot(data = damage, aes(x = "const", y = log10(TotalDamage + 1)))
+g <- g + geom_boxplot()
+g <- g + coord_flip()
+g
+
+
 table(damage$PROPDMGEXP)
 table(damage$CROPDMGEXP)
 
