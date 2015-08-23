13f5416e692cb6dc3a0ee2550d367de0db9eb862
nattsp <drnphillips@gmail.com>
2015-08-23 14:48:11 +1000

    Economic consequences data.frame part finished.

--- play.R
+++ play.R
@@ -38,6 +38,52 @@ head(harmful)
 harmful2 <- group_by(harmful, EVTYPE,) %>%
     summarise(Fatalies = sum(FATALITIES))
 
+#Across the United States, 
+#which types of events have the greatest economic consequences?
+
+table(storms$PROPDMGEXP)
+table(storms$CROPDMGEXP)
+
+convertPROPDMGEXP <- data.frame(PROPDMGEXP = c("H", "K", "M", "B", ""), 
+                           PROPDMGfactor = c(100, 1000, 1000000, 1000000000, ""))
+convertCROPDMGEXP <- data.frame(CROPDMGEXP = c("H", "K", "M", "B", ""), 
+                                PROPDMGfactor = c(100, 1000, 1000000, 1000000000, ""))
+
+
+damage <- select(storms,
+                EVTYPE,
+                PROPDMG,
+                PROPDMGEXP,
+                CROPDMG,
+                CROPDMGEXP) %>%
+    mutate_each(funs(toupper), PROPDMGEXP, CROPDMGEXP) %>%
+    filter(PROPDMGEXP %in% c("H", "K", "M", "B") |
+                         CROPDMGEXP %in% c("H", "K", "M", "B")) %>%
+    filter(PROPDMGEXP %in% c("H", "K", "M", "B", "") &
+                     CROPDMGEXP %in% c("H", "K", "M", "B", "")) %>%
+    left_join(convertPROPDMGEXP, c("PROPDMGEXP")) %>%
+    left_join(convertCROPDMGEXP, c("CROPDMGEXP"))
+
+
+head(damage)
+damage <- left_join(damage, convertCROPDMGEXP, c("CROPDMGEXP"))
+
+damage <- filter(damage, PROPDMGEXP %in% c("H", "K", "M", "B") |
+                     CROPDMGEXP %in% c("H", "K", "M", "B"))
+damage <- filter(damage, PROPDMGEXP %in% c("H", "K", "M", "B", "") &
+                     CROPDMGEXP %in% c("H", "K", "M", "B", ""))
+table(damage$PROPDMGEXP)
+table(damage$CROPDMGEXP)
+
+
+filter(damage, PROPDMGEXP >= 0, PROPDMGEXP < 9)
+filter(damage, PROPDMGEXP =="?")
+
+
+
+
+summary(damage)
+
 
 
 #902297 rows if not specified
